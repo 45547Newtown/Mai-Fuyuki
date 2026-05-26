1de883e5625411d5e057c5af222d12196bf97fa5
45547Newtown <neatly-mumbo-rule@duck.com>
2026-05-26 07:32:33 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,9 +1,16 @@
 from handlers.start import register_handlers
+from handlers.group_commands import register_group_commands
+
+from plugin.lock_system import register_all_lock_plugins
+from plugin.welcome_system import register_welcome_system
 from plugin.group_guard import register_group_guard
 
 
 def register_all_handlers(app):
-
     register_handlers(app)
-
+    register_group_commands(app)
+    register_all_lock_plugins(app)
+    register_welcome_system(app)
     register_group_guard(app)
+    print("✅ All handlers loaded successfully")
+    
