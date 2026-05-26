452b61032bbd222599b07cd01ad6659cef5f5b9a
45547Newtown <neatly-mumbo-rule@duck.com>
2026-05-26 06:41:40 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,16 @@
 from handlers.start import register_handlers
+
 from plugin.lock_system import register_all_lock_plugins
+from plugin.welcome_system import register_welcome_system
 
 
 def register_all_handlers(app):
 
-    # Start commands
+    # Start system
     register_handlers(app)
 
     # Lock system
     register_all_lock_plugins(app)
+
+    # Welcome system
+    register_welcome_system(app)
